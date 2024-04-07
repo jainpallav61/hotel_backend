a21e298e446d0eeeb9455d8fa5ee17d8a97e6785
Pallav Jain <jainpallav61@gmail.com>
2024-04-07 19:49:31 +0530

    Shifted mongodb to online server and added .env file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const db = require('./db');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000
 
 app.get('/',function(req,res){
     res.send('Welcome to my hotel');
@@ -16,6 +18,7 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person',presonRoutes); 
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000, ()=>{
+
+app.listen(PORT, ()=>{
     console.log('Listening to port 3000')
 });
